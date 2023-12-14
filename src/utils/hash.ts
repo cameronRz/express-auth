eab2b18a8d16c0df2aadc1cdb4274a800e5daa1e
Cameron Ramirez <caribcoded@gmail.com>
2023-12-14 15:54:38 -0500

    Update hash.ts

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
 
-export const hashPassword = async (password: string) => {
+export async function hashPassword(password: string) {
     return await bcrypt.hash(password, 10);
-};
+}
 
-export const comparePassword = async (password: string, hashedPassword: string) => {
+export async function comparePassword(password: string, hashedPassword: string) {
     return await bcrypt.compare(password, hashedPassword);
-};
+}
